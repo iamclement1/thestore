f4b769811f8a54728836188eb628cefc79f0cd75
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-03-17 12:54:44 +0100

    added folders and created files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Homepage from './Pages/Homepage';
 
 const App = () => {
     return (
         <div>
             <h1>Clement Store</h1>
+            <Homepage/>
             <button className='btn btn-primary'>LOGIN</button>
         </div>
     
