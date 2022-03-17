bac77379c834f54dfbfa1ad39077c36ea7269400
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-03-17 13:02:40 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Homepage from './Pages/Homepage';
+import RegisterPage from './Pages/RegisterPage';
+import LoginPage from './Pages/LoginPage';
+import ProductInfo from './Pages/ProductInfo';
+import CartPage from './Pages/CartPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
-            <h1>Clement Store</h1>
-            <Homepage/>
-            <button className='btn btn-primary'>LOGIN</button>
+        <div className='App'>
+            <BrowserRouter>
+            <Routes>
+                <Route path='/' exact element={<Homepage/>} />
+                <Route path='/register' element={<RegisterPage />} />
+                <Route path='/login' element={ <LoginPage /> } />
+                <Route path='/product-info' element={ <ProductInfo /> } />
+                <Route path='/cart' element={ <CartPage /> } />
+            </Routes>
+            </BrowserRouter>
         </div>
     
     )
