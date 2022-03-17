6b7c9f82defc39191b4f2f72adb2ba170ab18431
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-03-17 23:01:19 +0100

    added the footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
       <nav className='navbar navbar-expand-lg navbar-light bg-light'>
         <div className="container-fluid">
           <Link to='/' className="navbar-brand">
