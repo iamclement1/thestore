a9c2d5251802d67407687bee599c80b81f494c28
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-03-20 15:43:26 +0100

    added redux to save product state

---
+++ src/Redux/store.jsx
@@ -0,0 +1,6 @@
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./rootReducer";
+
+const composeEnhancers = composeWithDevTools( {} );
+export const store = createStore( rootReducer, composeEnhancers() ); 
