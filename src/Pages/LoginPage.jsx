09968152b428eb1a6eb718eaee281541ae5f3dbe
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-03-23 20:54:30 +0100

    added login page and styled the login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 function LoginPage() {
+
+  const [email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   return (
-    <div>LoginPage</div>
+    <div className='login-parent'>
+      <div className="login-top"></div>
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <div className="login-form">
+            <h2>Login</h2>
+            <hr />
+            {/* user name input */}
+            <input type="text" className="form-control" 
+            placeholder="Enter your email address"
+            value={email} onChange= {(e) => {setEmail (e.target.value);}} />
+
+            {/* password input box */}
+            <input type="password" className="form-control"
+            placeholder="Enter your password"
+            value={password} onChange= {(e) => {setPassword (e.target.value);}}/>
+
+            <button className='my-3 '> Login </button>
+          </div>
+        </div>
+        <div className="col-md-5">
+          <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_hsojyr3p.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>
+        </div>
+      </div>
+    </div>
   )
 }
 
